95c31a96fc1e576f6a991b68bae5af879f02132b
Haffi Mazhar <hmazhar@bink.com>
2023-02-01 09:52:08 +0000

    BPL-911: Account delete action

--- admin/views/accounts/main.py
+++ admin/views/accounts/main.py
@@ -1,5 +1,25 @@
+from datetime import datetime, timezone
+from typing import Generator, NamedTuple
+from uuid import UUID
+
+from flask import flash
+from flask_admin.actions import action
+from sqlalchemy import delete
+from sqlalchemy.orm import joinedload
+
+from admin.activity_utils.enums import ActivityType
 from admin.helpers.custom_formatters import account_holder_repr, campaign_slug_repr
 from admin.views.model_views import BaseModelView
+from cosmos.core.activity.tasks import sync_send_activity
+from cosmos.db.models import AccountHolder, Retailer
+from cosmos.retailers.enums import RetailerStatuses
+
+
+class AccountDeletedData(NamedTuple):
+    account_holder_uuid: UUID
+    retailer_name: str
+    retailer_status: RetailerStatuses
+    retailer_slug: str
 
 
 class AccountHolderAdmin(BaseModelView):
@@ -19,11 +39,66 @@ class AccountHolderAdmin(BaseModelView):
         "rewards",
         "marketing_preferences",
     )
-    column_labels = {"retailerconfig": "Retailer"}
+    column_labels = {"retailer": "Retailer"}
     column_searchable_list = ("id", "email", "account_holder_uuid", "account_number")
     form_widget_args = {
         "opt_out_token": {"readonly": True},
     }
+
+    def _generate_payloads_for_delete_account_holder_activity(
+        self,
+        deleted_accounts_data: list[AccountDeletedData],
+        sso_username: str,
+    ) -> Generator[dict, None, None]:  # pragma: no cover
+
+        return (
+            ActivityType.get_account_holder_deleted_activity_data(
+                activity_datetime=datetime.now(tz=timezone.utc),
+                account_holder_uuid=str(deleted_account.account_holder_uuid),
+                retailer_name=deleted_account.retailer_name,
+                retailer_status=deleted_account.retailer_status.name,
+                retailer_slug=deleted_account.retailer_slug,
+                sso_username=sso_username,
+            )
+            for deleted_account in deleted_accounts_data
+        )
+
+    @action(
+        "delete-account-holder",
+        "Delete",
+        "The selected account holders' retailer must be in a TEST state. "
+        "This action is not reversible, are you sure you wish to proceed?",
+    )
+    def delete_account_holder(self, ids: list[str]) -> None:
+        activity_payloads: Generator[dict, None, None] | None = None
+        account_holders_ids = [int(ah_id) for ah_id in ids]
+
+        result: list[AccountDeletedData] = self.session.execute(
+            delete(AccountHolder)
+            .options(joinedload(Retailer))
+            .where(
+                AccountHolder.id.in_(account_holders_ids),
+                AccountHolder.retailer_id == Retailer.id,
+                Retailer.status == RetailerStatuses.TEST,
+            )
+            .returning(
+                AccountHolder.account_holder_uuid,
+                Retailer.name,
+                Retailer.status,
+                Retailer.slug,
+            )
+            .execution_options(synchronize_session=False)
+        ).all()
+
+        if not result:
+            flash("This action is allowed only for account holders that belong to a TEST retailer.", category="error")
+            return
+
+        activity_payloads = self._generate_payloads_for_delete_account_holder_activity(result, self.sso_username)
+        sync_send_activity(activity_payloads, routing_key=ActivityType.ACCOUNT_DELETED.value)
+        self.session.commit()
+
+        flash(f"Deleted {len(result)} Account Holders.")
 
 
 class AccountHolderProfileAdmin(BaseModelView):
@@ -65,7 +140,7 @@ class CampaignBalanceAdmin(BaseModelView):
     can_create = False
     column_searchable_list = ("account_holder.id", "account_holder.email", "account_holder.account_holder_uuid")
     column_labels = {"account_holder": "Account Holder"}
-    column_filters = ("account_holder.retailer.slug", "campaign.slug")
+    column_filters = ("account_holder.retailer.slug", "campaign.slug", "reset_date")
     column_formatters = {"account_holder": account_holder_repr, "campaign": campaign_slug_repr}
     form_widget_args = {"account_holder": {"disabled": True}}
 
---
+++ tests/admin/views/accounts/test_account_holder_admin.py
@@ -0,0 +1,190 @@
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING, Callable
+
+from pytest_mock import MockerFixture
+from sqlalchemy.future import select
+
+from admin.views.accounts import AccountHolderAdmin
+from cosmos.accounts.enums import MarketingPreferenceValueTypes
+from cosmos.db.models import (
+    AccountHolder,
+    AccountHolderProfile,
+    CampaignBalance,
+    MarketingPreference,
+    PendingReward,
+    Retailer,
+    Reward,
+    Transaction,
+)
+from cosmos.retailers.enums import RetailerStatuses
+from tests.conftest import SetupType
+
+if TYPE_CHECKING:
+    from flask.testing import FlaskClient
+    from sqlalchemy.orm import Session
+    from werkzeug import Response
+
+
+def _fetch_account_holders(db_session: "Session") -> list[AccountHolder]:
+    return db_session.execute(select(AccountHolder)).scalars().all()
+
+
+def mock_delete_account_holders_request(account_holder_ids: list[str], client: "FlaskClient") -> "Response":
+    return client.post(
+        "/admin/accounts/account-holders/action/",
+        data={
+            "url": "/admin/accounts/account-holders/",
+            "action": "delete-account-holder",
+            "rowid": account_holder_ids,
+        },
+        follow_redirects=True,
+    )
+
+
+def test_delete_account_holder_action_invalid_retailer_status(
+    setup: SetupType,
+    test_client: "FlaskClient",
+) -> None:
+    """Tests deleting one account holder with invalid retailer status"""
+    db_session, retailer, _ = setup
+
+    retailer.status = RetailerStatuses.ACTIVE
+    db_session.commit()
+
+    fetched_account_holders = _fetch_account_holders(db_session)
+    assert len(fetched_account_holders) == 1
+
+    resp = mock_delete_account_holders_request([str(fetched_account_holders[0].id)], test_client)
+
+    assert resp.status_code == 200
+    assert (
+        "This action is allowed only for account holders that belong to a TEST retailer." in resp.text  # type: ignore
+    )
+
+    re_fetched_account_holders = _fetch_account_holders(db_session)
+    assert len(re_fetched_account_holders) == 1
+
+
+def test_delete_account_holder_action_success_with_cascades(
+    setup: SetupType,
+    test_client: "FlaskClient",
+    mocker: MockerFixture,
+    user_reward: Reward,
+    campaign_balance: CampaignBalance,
+    pending_reward: PendingReward,
+) -> None:
+    """Tests deleting one account holder and checking for all cascade deletes"""
+    db_session, retailer, account_holder = setup
+
+    mocker.patch.object(AccountHolderAdmin, "sso_username", "test-user")
+    mock_send_activity = mocker.patch("admin.views.accounts.main.sync_send_activity")
+
+    user_reward.account_holder_id = account_holder.id
+    tx = Transaction(
+        account_holder_id=account_holder.id,
+        retailer_id=retailer.id,
+        transaction_id="tx_id",
+        amount=300,
+        mid="mid",
+        datetime=datetime.now(tz=timezone.utc),
+    )
+    prefs = MarketingPreference(
+        account_holder_id=account_holder.id,
+        key_name="marketing_pref",
+        value="True",
+        value_type=MarketingPreferenceValueTypes.BOOLEAN,
+    )
+    db_session.add(tx)
+    db_session.add(prefs)
+    db_session.commit()
+
+    fetched_account_holders = _fetch_account_holders(db_session)
+    assert len(fetched_account_holders) == 1
+    assert fetched_account_holders[0].profile is not None
+    assert fetched_account_holders[0].pending_rewards is not None
+    assert fetched_account_holders[0].rewards is not None
+    assert fetched_account_holders[0].current_balances is not None
+    assert fetched_account_holders[0].transactions is not None
+    assert fetched_account_holders[0].marketing_preferences is not None
+    assert fetched_account_holders[0].retailer.status == RetailerStatuses.TEST
+
+    resp = mock_delete_account_holders_request([str(fetched_account_holders[0].id)], test_client)
+
+    assert resp.status_code == 200
+    assert f"Deleted {len(fetched_account_holders)} Account Holders." in resp.text  # type: ignore
+
+    re_fetched_account_holders = _fetch_account_holders(db_session)
+    assert not re_fetched_account_holders
+
+    # Check for cascade deletes
+    for table in (AccountHolderProfile, PendingReward, Reward, CampaignBalance, Transaction, MarketingPreference):
+        assert len(db_session.execute(select(table)).scalars().all()) == 0
+
+    # Check activity was sent
+    mock_send_activity.assert_called_once()
+
+
+def test_delete_account_holder_action_multiple(
+    setup: SetupType,
+    test_client: "FlaskClient",
+    mocker: MockerFixture,
+    create_account_holder: Callable[..., AccountHolder],
+) -> None:
+    """Tests deleting multiple account holders successfully"""
+
+    db_session, _, _ = setup
+
+    mocker.patch.object(AccountHolderAdmin, "sso_username", "test-user")
+    mock_send_activity = mocker.patch("admin.views.accounts.main.sync_send_activity")
+
+    create_account_holder(email="other@account.holder")
+
+    fetched_account_holders = _fetch_account_holders(db_session)
+    assert len(fetched_account_holders) == 2
+
+    resp = mock_delete_account_holders_request(
+        [str(fetched_account_holders[0].id), str(fetched_account_holders[1].id)], test_client
+    )
+
+    assert resp.status_code == 200
+    assert f"Deleted {len(fetched_account_holders)} Account Holders." in resp.text  # type: ignore
+
+    re_fetched_account_holders = _fetch_account_holders(db_session)
+    assert not re_fetched_account_holders
+
+    # Check activity was sent
+    mock_send_activity.assert_called_once()
+
+
+def test_delete_account_holder_action_multiple_with_one_invalid(
+    setup: SetupType,
+    test_client: "FlaskClient",
+    mocker: MockerFixture,
+    create_account_holder: Callable[..., AccountHolder],
+    create_retailer: Callable[..., Retailer],
+) -> None:
+    """Tests deleting one account with active retailer, one account with test retailer"""
+
+    db_session, _, _ = setup
+
+    mocker.patch.object(AccountHolderAdmin, "sso_username", "test-user")
+    mock_send_activity = mocker.patch("admin.views.accounts.main.sync_send_activity")
+
+    new_retailer = create_retailer(name="new retailer", slug="new-retailer", status=RetailerStatuses.ACTIVE)
+    create_account_holder(email="other@account.holder", retailer_id=new_retailer.id)
+
+    fetched_account_holders = _fetch_account_holders(db_session)
+    assert len(fetched_account_holders) == 2
+
+    resp = mock_delete_account_holders_request(
+        [str(fetched_account_holders[0].id), str(fetched_account_holders[1].id)], test_client
+    )
+
+    assert resp.status_code == 200
+    assert "Deleted 1 Account Holders." in resp.text  # type: ignore
+
+    re_fetched_account_holders = _fetch_account_holders(db_session)
+    assert len(re_fetched_account_holders) == 1
+
+    # Check activity was sent
+    mock_send_activity.assert_called_once()
