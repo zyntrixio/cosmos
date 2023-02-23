4884b5e347c110bd978050810dab8f458948b612
Stewart Perrygrove <sperrygrove@bink.com>
2023-02-23 11:32:09 +0000

    BPL-984 Add app label to metrics that need it

--- cosmos/core/prometheus.py
+++ cosmos/core/prometheus.py
@@ -28,13 +28,13 @@ tasks_run_total = Counter(
 invalid_marketing_opt_out = Counter(
     name=f"{METRIC_NAME_PREFIX}erroneous_marketing_opt_out_get_requests",
     documentation="The current number of bad requests",
-    labelnames=("unknown_retailer", "invalid_token"),
+    labelnames=("app", "unknown_retailer", "invalid_token"),
 )
 
 microsite_reward_requests = Counter(
     name=f"{METRIC_NAME_PREFIX}erroneous_rewards_for_microsite_get_requests",
     documentation="The current number of requests",
-    labelnames=("response_status", "unknown_retailer", "invalid_reward_uuid"),
+    labelnames=("app", "response_status", "unknown_retailer", "invalid_reward_uuid"),
 )
 
 task_statuses = Gauge(
