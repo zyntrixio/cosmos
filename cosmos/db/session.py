6411fe80dae807015ee10fbc41bdd4926be2279c
Stewart Perrygrove <sperrygrove@bink.com>
2023-04-21 12:39:14 +0100

    Bugfix: Fix engine arguments for pooled database connections
    Bug introduced in a81cafb58

--- cosmos/db/session.py
+++ cosmos/db/session.py
@@ -5,19 +5,14 @@ from sqlalchemy.pool import NullPool
 
 from cosmos.db.config import db_settings
 
-engine_kwargs = (
-    {
-        "connect_args": {"application_name": "cosmos"},
-        "pool_pre_ping": True,
-        "echo": db_settings.SQL_DEBUG,
-    }
-    | {"poolclass": NullPool}
-    if db_settings.USE_NULL_POOL or db_settings.TESTING
-    else {}
-)
+engine_kwargs = {
+    "connect_args": {"application_name": "cosmos"},
+    "pool_pre_ping": True,
+    "echo": db_settings.SQL_DEBUG,
+} | ({"poolclass": NullPool} if db_settings.USE_NULL_POOL or db_settings.TESTING else {})
 
 async_engine = create_async_engine(db_settings.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
 sync_engine = create_engine(db_settings.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
-AsyncSessionMaker = async_sessionmaker(async_engine, future=True, expire_on_commit=False)
-SyncSessionMaker = sessionmaker(sync_engine, future=True, expire_on_commit=False)
+AsyncSessionMaker = async_sessionmaker(async_engine, expire_on_commit=False)
+SyncSessionMaker = sessionmaker(sync_engine, expire_on_commit=False)
 scoped_db_session = scoped_session(sessionmaker(bind=sync_engine))  # For Flask-Admin
