zyntrixio/cosmos

commit a81cafb58044193d3aca4d899398a07ceb6ddeba

Upgrade SQLAlchemy to version 2.x

cosmos/db/session.py
@@ -1,27 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import NullPool
 
 from cosmos.db.config import db_settings
 
-null_pool = {"poolclass": NullPool} if db_settings.USE_NULL_POOL or db_settings.TESTING else {}  # pragma: no cover
-
-# application_name
-CONNECT_ARGS = {"application_name": "cosmos"}
-
-# future=True enables sqlalchemy core 2.0
-async_engine = create_async_engine(
-    db_settings.SQLALCHEMY_DATABASE_URI_ASYNC, pool_pre_ping=True, future=True, echo=db_settings.SQL_DEBUG, **null_pool
+engine_kwargs = (
+    {
+        "connect_args": {"application_name": "cosmos"},
+        "pool_pre_ping": True,
+        "echo": db_settings.SQL_DEBUG,
+    }
+    | {"poolclass": NullPool}
+    if db_settings.USE_NULL_POOL or db_settings.TESTING
+    else {}
 )
-sync_engine = create_engine(
-    db_settings.SQLALCHEMY_DATABASE_URI,
-    connect_args=CONNECT_ARGS,
-    pool_pre_ping=True,
-    echo=db_settings.SQL_DEBUG,
-    future=True,
-    **null_pool
-)
-AsyncSessionMaker = sessionmaker(bind=async_engine, future=True, expire_on_commit=False, class_=AsyncSession)
-SyncSessionMaker = sessionmaker(bind=sync_engine, future=True, expire_on_commit=False)
+
+async_engine = create_async_engine(db_settings.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
+sync_engine = create_engine(db_settings.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
+AsyncSessionMaker = async_sessionmaker(async_engine, future=True, expire_on_commit=False)
+SyncSessionMaker = sessionmaker(sync_engine, future=True, expire_on_commit=False)
 scoped_db_session = scoped_session(sessionmaker(bind=sync_engine))  # For Flask-Admin

admin/hubble/db/session.py
@@ -13,4 +13,4 @@ if TYPE_CHECKING:
 
 activity_engine = create_engine(admin_settings.ACTIVITY_SQLALCHEMY_URI, poolclass=NullPool)
 SessionMaker = sessionmaker(bind=activity_engine, future=True, expire_on_commit=False)
-activity_scoped_session: "Session" = scoped_session(SessionMaker)
+activity_scoped_session: scoped_session["Session"] = scoped_session(SessionMaker)

commit 2c8b6a521f88f869d9b81cfc6890092031389f59

Bugfix: Fix engine arguments for pooled database connections
Bug introduced in a81cafb58

cosmos/db/session.py
@@ -5,19 +5,14 @@ from sqlalchemy.pool import NullPool
 
 from cosmos.db.config import db_settings
 
-engine_kwargs = (
-    {
-        "connect_args": {"application_name": "cosmos"},
-        "pool_pre_ping": True,
-        "echo": db_settings.SQL_DEBUG,
-    }
-    | {"poolclass": NullPool}
-    if db_settings.USE_NULL_POOL or db_settings.TESTING
-    else {}
-)
+engine_kwargs = {
+    "connect_args": {"application_name": "cosmos"},
+    "pool_pre_ping": True,
+    "echo": db_settings.SQL_DEBUG,
+} | ({"poolclass": NullPool} if db_settings.USE_NULL_POOL or db_settings.TESTING else {})
 
 async_engine = create_async_engine(db_settings.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
 sync_engine = create_engine(db_settings.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
-AsyncSessionMaker = async_sessionmaker(async_engine, future=True, expire_on_commit=False)
-SyncSessionMaker = sessionmaker(sync_engine, future=True, expire_on_commit=False)
+AsyncSessionMaker = async_sessionmaker(async_engine, expire_on_commit=False)
+SyncSessionMaker = sessionmaker(sync_engine, expire_on_commit=False)
 scoped_db_session = scoped_session(sessionmaker(bind=sync_engine))  # For Flask-Admin